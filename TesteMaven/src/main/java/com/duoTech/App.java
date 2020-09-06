d462e1d61014fcbef5f8a3fdef1d3906ca2d2aa9
DESKTOP-5LSHH6H\Pichau <eric_carlotto@estudante.sc.senai.br>
2020-09-06 00:01:57 -0300

    Criado a área para o felipe comentar.

--- TesteMaven/src/main/java/com/duoTech/App.java
+++ TesteMaven/src/main/java/com/duoTech/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("O Eric criou e mexeu no projeto dia 05/09");
+        System.out.println("FELIPE ESCREVE AQUI");
     }
 }
