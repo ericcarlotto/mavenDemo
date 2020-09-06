8a50e3f59e07353fd21a9fa780c097960569ba93
DESKTOP-5LSHH6H\Pichau <eric_carlotto@estudante.sc.senai.br>
2020-09-05 23:58:57 -0300

    Comecando projetomvn

---
+++ TesteMaven/src/main/java/com/duoTech/App.java
@@ -0,0 +1,14 @@
+package com.duoTech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("O Eric criou e mexeu no projeto dia 05/09");
+    }
+}
